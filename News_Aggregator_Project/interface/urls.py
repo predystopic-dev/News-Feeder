1402f666dd88242790e9db0e725ea0b184563893
Dev <github@jamesthomas.anonaddy.com>
2023-05-09 09:39:41 +0530

    added static for user_registration app and removed unnecessary comments

--- News_Aggregator_Project/interface/urls.py
+++ News_Aggregator_Project/interface/urls.py
@@ -4,6 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.display_csv, name="display_csv"),
     path('', views.interface, name="interface"),
-    # path('login', views.my_login, name="login"),
     path('bias', views.bias, name="bias"),
 ]
